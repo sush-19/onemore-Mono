f3923500d0a49db70282d7695884e17d5659793f
root <root@ip-172-31-57-113.ec2.internal>
2023-02-04 15:40:56 +0000

    all terrform files have been added

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+region = "us-east-1"
+access_key = "AKIA3FKIX4335XVU5F7G"
+secret_key = "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"
+}
+
+resource "aws_instance" "key" {
+ami = "ami-0aa7d40eeae50c9a9"
+instance_type = "t2.micro"
+vpc_security_group_ids = [aws_security_group.demo-sg.id]
+tags = {
+Name = "mani"
+}
+}
---
+++ terraform/security.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "demo-sg" {
+  name = "sushma"
+  description = "Allow HTTP and SSH traffic via Terraform"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
